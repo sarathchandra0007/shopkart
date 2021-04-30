9823ad7f4a50c6debfeec95d4471935793007a09
Sarath <sarathchandra0007@gmail.com>
2021-04-30 23:58:45 +0530

    Implemented store feature

--- store/views.py
+++ store/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+from category.models import Category
 # Create your views here.
+
+
+def store(request, category_slug=None):
+    if category_slug != None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=categories, is_available=True)
+    else:
+        products = Product.objects.all().filter(is_available=True)
+    context = {
+        'products': products,
+        'product_count': products.count()
+    }
+    return render(request, 'store/store.html', context=context)
