71c1bff7d25251ed5680b87926a411d80fb98685
Sarath <sarathchandra0007@gmail.com>
2021-05-06 21:41:51 +0530

    Added pagination to site

--- cart/views.py
+++ cart/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from store.models import Product
 from .models import Cart, CartItem
+from django.core.exceptions import ObjectDoesNotExist
 # Create your views here.
 
 
@@ -13,7 +14,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
             quantity += cart_item.quantity
         tax = (2*total)/100
         grand_total = total + tax
-    except ObjectNotExist:
+    except ObjectDoesNotExist:
         pass
     context = {
         'total': total,
